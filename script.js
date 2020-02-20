c838718beb1eb16b28d98626569a15ce589a6278
DrunkenPoney_Qc <drunkenponey@gmail.com>
2020-02-19 19:26:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'BONNE', 'FÊTE', 'GABRIELLE!!!' ],
+			strings: [ 'HAPPY', 'BIRTHDAY', 'CARLSAGAN42!!!' ],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
