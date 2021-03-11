75e7a5ebd38655bc5eb01773096c36ac74981434
DrunkenPoney (SCT) <drunkenponey@gmail.com>
2021-03-11 13:00:12 -0500

    font-awesome

--- congrats/script.js
+++ congrats/script.js
@@ -1,3 +1,5 @@
+const prepareFontLoad = (fontList) => Promise.all(fontList.map(font => document.fonts.load(font)));
+
 (async () => {
   const c    = document.getElementById('c');
   let params = location.search.substr(1)
@@ -8,6 +10,8 @@
   let msg = (await import('./messages.js')).default;
   msg     = msg[params.msg] || msg['congrats'];
   
+  await prepareFontLoad(['Font Awesome 5 Free', 'Font Awesome 5 Brands']);
+  
   const { animation } = await import(`./animations/${msg.animation}.js`);
   
   msg = msg.message[params.lang] || msg.message['en'] || msg.message['fr'];
@@ -15,6 +19,6 @@
       m.replace(/{([\w\-]+)}/g, (m, g) => params[g] || m)
        .replace(/{utf:([0-9A-Z]+)}/gi, (m, g) => String.fromCharCode(parseInt(g, 16)))
        .split('\n'));
-  console.log(msg);
+  
   animation(msg, c);
 })()
